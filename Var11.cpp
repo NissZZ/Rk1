f62e48ee57997eb380fbbb228b4d688d893de15c
Matvey Ivanov <98405814+NissZZ@users.noreply.github.com>
2024-10-05 11:50:29 +0300

    Create Var11.cpp

---
+++ Var11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    double summ = 0, el = 0;
+    for(int n = 1; n < 11; ++n)
+    {
+        el = 1/(pow(n, 2) + pow(n, (1/3)));
+        summ += el;
+        cout << "1)" << el << endl;
+    }
+    cout << "summ="<< summ << endl;
+    return 0;
+}
